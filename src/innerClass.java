4d279d739ea825a3bf9a9ab4103716a5bd3916b0
shaiq19 <shaiq.tanveer@gmail.com>
2022-07-22 18:23:35 +0500

    staticinner clas

--- src/innerClass.java
+++ src/innerClass.java
@@ -4,7 +4,8 @@ int number =6;
 public void hellothere(){
     System.out.println("hello from outer class");
 }
-    public class InnerClass{
+
+    public static class InnerClass{
 
     int innernumber = 10;
     public void innerhellothere(){
@@ -16,7 +17,7 @@ public void hellothere(){
 public static void main(String[] args){
     OuterClass o= new OuterClass();
     o.hellothere();
-    OuterClass.InnerClass i = o.new  InnerClass();
+    OuterClass.InnerClass i = new  OuterClass.InnerClass();
     i.innerhellothere();
 
 }
